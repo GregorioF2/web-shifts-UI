cd52c27590a52939cdedb68bd40af7a7b15468ed
gregorio <gfreidin@dc.uba.ar>
2020-05-20 19:34:55 -0300

    Add main buttons

---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-button class="button-div" variant="outline-primary">{{title}}</b-button>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Button",
+    }
+  },
+};
+</script>
+
+<style scoped>
+.main-container {
+  
+}
+.button-div {
+  margin: auto;
+  height: 300px;
+  width: 700px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="home">
-    <h1>Hola Turris</h1>
+    <qr-button :title="'button 1'"></qr-button>
+    <qr-button :title="'button 2'"></qr-button>
   </div>
 </template>
 
 <script>
-
+import QrButton from "../components/Button";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-  }
-}
+    QrButton,
+  },
+};
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
