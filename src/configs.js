ae934620c1fac5148931a6001372f659b3cf5dd1
gregorio <gfreidin@dc.uba.ar>
2020-06-06 17:21:44 -0300

    Use https

--- src/configs.js
+++ src/configs.js
@@ -1,3 +1,3 @@
 exports.OWNER_USER_TYPE = 'propietario';
 exports.CLIENT_USER_TYPE = 'cliente';
-exports.SERVER_URL = 'http://arqweb-concept-queue.herokuapp.com';
+exports.SERVER_URL = 'https://arqweb-concept-queue.herokuapp.com';
